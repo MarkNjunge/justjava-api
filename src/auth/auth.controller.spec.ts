0f48f0dfe0ed1dcb7643aa804932b73209749d7d
MarkNjunge <mark.kamau@outlook.com>
2019-11-21 22:16:06 +0300

    Add missing test dependencies

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,5 +1,10 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { UserEntity } from "./entities/User.entity";
+import { Repository } from "typeorm";
+import { RedisService } from "../redis/redis.service";
 
 describe("Auth Controller", () => {
   let controller: AuthController;
@@ -7,6 +12,17 @@ describe("Auth Controller", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        AuthService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useClass: Repository,
+        },
+        {
+          provide: RedisService,
+          useClass: jest.fn(),
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
+import { RedisService } from "../redis/redis.service";
+import { UserEntity } from "./entities/User.entity";
+import { Repository } from "typeorm";
+import { getRepositoryToken } from "@nestjs/typeorm";
 
 describe("AuthService", () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: RedisService,
+          useClass: jest.fn(),
+        },
+        {
+          provide: getRepositoryToken(UserEntity),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
