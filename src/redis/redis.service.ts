be1728d4bf911b7cde532e82a443e46e4eaea3a7
MarkNjunge <mark.kamau@outlook.com>
2019-11-22 09:55:04 +0300

    Move redis connection outside of constructor
    Tests were never ending because it was continuously trying to connect to Redis.

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,21 +1,61 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, HttpStatus } from "@nestjs/common";
 import * as IoRedis from "ioredis";
 import { config } from "../common/Config";
 import { SessionDto } from "src/auth/dto/Session.dto";
+import { CustomLogger } from "../common/CustomLogger";
+import { ApiException } from "../common/ApiException";
 
 @Injectable()
 export class RedisService {
+  private isConnected = false;
   private redis: IoRedis.Redis;
+  private logger: CustomLogger;
 
   constructor() {
+    this.logger = new CustomLogger("RedisService");
+  }
+
+  async connect() {
     this.redis = new IoRedis({
       host: config.redis.host,
       port: config.redis.port,
       password: config.redis.password,
+      retryStrategy() {
+        return false;
+      },
+    });
+
+    this.redis.on("connect", () => {
+      this.logger.log("Connected to Redis instance");
+      this.isConnected = true;
+    });
+
+    this.redis.on("error", err => {
+      if (err.code === "ECONNREFUSED") {
+        this.logger.error(
+          "Unable to connect to Redis instance. Authentication will not work.",
+          "RedisService.connect",
+        );
+        this.isConnected = false;
+      } else {
+        this.logger.error(err.message, "RedisService.connect");
+      }
     });
   }
 
+  private assertHasConnected() {
+    if (!this.isConnected) {
+      throw new ApiException(
+        HttpStatus.INTERNAL_SERVER_ERROR,
+        "Unable to authenticate",
+        { reason: "A connection to Redis has not been established" },
+      );
+    }
+  }
+
   async saveSession(session: SessionDto) {
+    this.assertHasConnected();
+
     await this.redis.set(
       `session:${session.userId}:${session.sessionId}`,
       JSON.stringify(session),
@@ -23,6 +63,8 @@ export class RedisService {
   }
 
   async getSession(sessionId: string): Promise<SessionDto> {
+    this.assertHasConnected();
+
     const res = await this.redis.scan(0, "MATCH", `session:*:${sessionId}`);
 
     if (res[1][0]) {
@@ -34,11 +76,15 @@ export class RedisService {
   }
 
   async updateLastUseDate(session: SessionDto) {
+    this.assertHasConnected();
+
     session.lastUseDate = Math.floor(Date.now() / 1000);
     await this.saveSession(session);
   }
 
   async deleteSession(session: SessionDto) {
+    this.assertHasConnected();
+
     await this.redis.del(`session:${session.userId}:${session.sessionId}`);
   }
 }
