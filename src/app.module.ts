5e7c173aa25ed291b88757b3b9a6b73c6ca1eeee
MarkNjunge <mark.kamau@outlook.com>
2020-03-28 10:47:24 +0300

    Clean up module dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,6 @@ import { AppController } from "./app/app.controller";
 import { AppService } from "./app/app.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { config } from "./common/Config";
-import { ProductEntity } from "./shared/products/entities/Product.entity";
-import { UserEntity } from "./shared/users/entities/User.entity";
-import { AddressEntity } from "./shared/users/entities/Address.entity";
-import { OrderEntity } from "./shared/orders/entities/Order.entity";
-import { PaymentEntity } from "./shared/payments/entities/Payment.entity";
 import { AdminModule } from "./admin/admin.module";
 import { SharedModule } from "./shared/shared.module";
 import { ClientModule } from "./client/client.module";
@@ -26,13 +21,6 @@ import * as path from "path";
         ssl: config.db.ssl,
       },
     }),
-    TypeOrmModule.forFeature([
-      ProductEntity,
-      UserEntity,
-      AddressEntity,
-      OrderEntity,
-      PaymentEntity,
-    ]),
     ClientModule,
     AdminModule,
     SharedModule,
--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -4,17 +4,13 @@ import { SharedModule } from "../shared/shared.module";
 import { AuthService } from "./auth/auth.service";
 import { AuthController } from "./auth/auth.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { ProductEntity } from "../shared/products/entities/Product.entity";
 import { UserEntity } from "../shared/users/entities/User.entity";
 import { UsersController } from "./users/users.controller";
 import { OrdersController } from "./orders/orders.controller";
 import { PaymentsController } from "./payments/payments.controller";
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([ProductEntity, UserEntity]),
-    SharedModule,
-  ],
+  imports: [TypeOrmModule.forFeature([UserEntity]), SharedModule],
   controllers: [
     ProductsController,
     AuthController,
