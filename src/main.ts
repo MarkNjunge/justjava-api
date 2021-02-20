6d9344f42205026d37d8306ef5da443f2e82ac47
MarkNjunge <mark.kamau@outlook.com>
2021-02-20 20:50:19 +0300

    Cleanup main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,7 @@ import { LoggingInterceptor } from "./common/interceptors/logging.interceptor";
 import { ValidationPipe } from "./common/pipes/validation.pipe";
 import { config } from "./common/Config";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from "@nestjs/platform-fastify";
+import { FastifyAdapter, NestFastifyApplication, } from "@nestjs/platform-fastify";
 import * as fastifyRateLimit from "fastify-rate-limit";
 import * as fileUpload from "fastify-file-upload";
 import { RedisService } from "./shared/redis/redis.service";
@@ -18,7 +15,7 @@ import { requestTimeMiddleware } from "./common/middleware/request-time.middlewa
 import { SetCookiesInterceptor } from "./common/interceptors/set-cookies.interceptor";
 import { default as helmet } from "fastify-helmet";
 
-declare const module: any;
+bootstrap().catch(e => console.error(e))
 
 async function bootstrap() {
   initializeWinston();
@@ -36,34 +33,8 @@ async function bootstrap() {
     },
   );
 
-  intializeSwagger(app);
-
-  await app.register(helmet, {
-    // A custom Content Security Policy config is required in order for swagger to work
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'"],
-        styleSrc: ["'self'", "'unsafe-inline'"],
-        imgSrc: ["'self'", "data:", "validator.swagger.io"],
-        scriptSrc: ["'self'", "https: 'unsafe-inline'"],
-      },
-    },
-  });
-
-  if (config.rateLimit.enabled === true) {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    app.register(fastifyRateLimit, {
-      max: config.rateLimit.max,
-      timeWindow: config.rateLimit.timeWindow,
-    });
-  }
-
-  app.enableCors({
-    origin: config.corsOrigin,
-    methods: config.corsMethods,
-    allowedHeaders: config.corsHeaders,
-  });
+  await enablePlugins(app)
+  initializeSwagger(app);
 
   app.useGlobalFilters(new AllExceptionsFilter());
   app.useGlobalInterceptors(new LoggingInterceptor());
@@ -80,17 +51,40 @@ async function bootstrap() {
   await notifications.connect();
 
   await app.listen(config.port, "0.0.0.0").then(() => {
-    logger.log(`Started on port ${config.port}`);
+    logger.log(`App running at http://127.0.0.1:${config.port}`);
+  });
+}
+
+async function enablePlugins(app: NestFastifyApplication){
+  await app.register(helmet, {
+    // A custom Content Security Policy config is required in order for swagger to work
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", "data:", "validator.swagger.io"],
+        scriptSrc: ["'self'", "https: 'unsafe-inline'"],
+      },
+    },
   });
 
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
+  app.enableCors({
+    origin: config.corsOrigin,
+    methods: config.corsMethods,
+    allowedHeaders: config.corsHeaders,
+  });
+
+  if (Boolean(config.rateLimit.enabled) === true) {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    app.register(fastifyRateLimit, {
+      max: config.rateLimit.max,
+      timeWindow: config.rateLimit.timeWindow,
+    });
   }
 }
-bootstrap();
 
-function intializeSwagger(app: NestFastifyApplication) {
+function initializeSwagger(app: NestFastifyApplication) {
   const options = new DocumentBuilder()
     .setTitle("JustJava API")
     .setDescription("JustJava API")
